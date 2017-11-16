405cef3a1b2ec8d55d316bb7d1276364354a2c21
shemlevy <shemyafe@gmail.com>
2017-11-16 19:15:40 +0200

    added global vars in js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 'use strict'
 console.log('memegenertor')
+
+var gImgs = [{ id: 1, url: 'img/gellary/1.jpg', keywords: ['happy'] }]; 
+var gMeme = {
+    selectedImgId: 5,
+    txts: [
+        {
+            line: 'I never eat Falafel', size: 20,
+            align: 'left',
+            color: 'red'
+        }]
+}
+
+
+
+
+
