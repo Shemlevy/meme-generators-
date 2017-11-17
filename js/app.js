9ab3e5c02cb095966ed565cb07d1e99af752f347
shemlevy <shemyafe@gmail.com>
2017-11-17 12:55:16 +0200

    good chang

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 console.log('memegenertor')
 
 //global vars
-var gImgs = [{ id: 1, url: 'img/gallery/1.jpg', keywords: ['happy', 'really happy'] },
+var gImgs = [{ id: 1, url: 'img/gallery/1.jpg', keywords: ['sad', 'really happy'] },
 { id: 2, url: 'img/gallery/2.jpg', keywords: ['angry', 'mad', 'smoke'] },
 { id: 2, url: 'img/gallery/2.jpg', keywords: ['angry', 'mad', 'smoke'] },
 { id: 2, url: 'img/gallery/2.jpg', keywords: ['angry', 'mad', 'smoke'] },
@@ -14,7 +14,7 @@ var gImgs = [{ id: 1, url: 'img/gallery/1.jpg', keywords: ['happy', 'really happ
 { id: 2, url: 'img/gallery/2.jpg', keywords: ['angry', 'mad', 'smoke'] },
 { id: 2, url: 'img/gallery/1.jpg', keywords: ['angry', 'mad', 'smoke'] },
 { id: 2, url: 'img/gallery/2.jpg', keywords: ['angry', 'mad', 'smoke'] },
-{ id: 2, url: 'img/gallery/1.jpg', keywords: ['angry', 'mad', 'smoke'] },
+{ id:12, url: 'img/gallery/12.jpg', keywords: ['sad', 'mad', 'smoke'] },
 ];
 
 
@@ -29,13 +29,13 @@ var gMeme = {
         }]
 };
 
-renderImgs();
+renderImgs(gImgs);
 //function render photo to gallery
-function renderImgs() {
-    var elGallery = document.querySelector('.gallery');
+function renderImgs(array) {
+    var elGallery = document.querySelector('.gallery'); //////chaned here to render every array
     var strHtml = ''
-    gImgs.forEach(function (img) {
-        console.log(img.url)
+    array.forEach(function (img) {
+        // console.log(img.url)
         return strHtml += `<img onclick="drawOnCanvas(${img.id})" class="img-gallery" src="${img.url}"  alt="">`
     });
     elGallery.innerHTML = strHtml;
@@ -43,10 +43,11 @@ function renderImgs() {
 
 //function draw selcted img on canvas and pass user to edit screen
 function drawOnCanvas(id) {
+    document.querySelector('.gallery').classList.add('gallery-hide');    //// do you think theres a better way?
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     var img = new Image();
-    console.log(id)
+    // console.log(id)
 
     img.src = `img/gallery/${id}.jpg`;
 
@@ -65,11 +66,13 @@ function getMemeBykey(key) {
         var img = gImgs[i];
         var x = img.keywords.filter(function (keyword) {
             if (keyword === key) {
-                imgUrl.push(img.url)
+                imgUrl.push(img)
             }
         });
     };
-    return imgUrl;
+    // return imgUrl;
+    // console.log('try')
+    renderImgs(imgUrl)
 }
 
 
